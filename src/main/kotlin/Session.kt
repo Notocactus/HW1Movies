6517376b576b92de029c308a9a91394ac0cf2a1c
notocactus <71428388+Notocactus@users.noreply.github.com>
2023-12-12 00:00:59 +0300

    Update Session.kt

--- src/main/kotlin/Session.kt
+++ src/main/kotlin/Session.kt
@@ -2,5 +2,5 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 class Session(val id: UInt, var movieId: UInt, var date: String, var cost: UInt) {
-
+    var places: Array<Byte> = Array<Byte>(100) { 0 }
 }
