5f1df8084bf7b3053abeb9cf101db6b227af39d0
Victoria Khan <vshkhan@edu.hse.ru>
2023-12-11 23:46:25 +0300

    1-4 operations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -18,6 +18,26 @@ fun isDateValid(date: String?): Boolean {
     }
 }
 
+fun isDurationValid(input : String?): Boolean {
+    val myFormat = SimpleDateFormat("HH:mm")
+    myFormat.isLenient = false
+    try {
+        myFormat.parse(input)
+        return true
+    } catch (e: Exception) {
+        return false
+    }
+}
+
+fun isUInt(input : String?): Boolean {
+    try {
+        var uInt : UInt = input!!.toUInt()
+        return true
+    } catch (e: NumberFormatException) {
+        return false
+    }
+}
+
 fun CheckCreate(files: Array<String>) {
     for (item in files) {
         var file = File(item)
@@ -26,9 +46,6 @@ fun CheckCreate(files: Array<String>) {
 }
 
 fun main() {
-
-//    val dateTime = LocalDateTime.now()
-//    println(dateTime.format(DateTimeFormatter.ofPattern("d/M/y H:m:ss")))
 
     CheckCreate(files)
 
@@ -40,6 +57,7 @@ fun main() {
         command = readln()
         when (command) {
             "0" -> break
+
             "1" -> {
                 println("Введите дату и время сеанса (DD/MM/YYYY HH:MM): ")
                 var sessionDate : String = readln()
@@ -51,22 +69,57 @@ fun main() {
                 var place : String = readln()
                 var placeNum : UInt
                 while (true) {
-                    try {
+                    if (isUInt(place)) {
                         placeNum = place.toUInt()
-                        if (placeNum == 0u || placeNum > 100u) {
-                            throw NumberFormatException()
+                        if (placeNum > 1u && placeNum <= 100u) {
+                            break
                         }
-                        break
-                    } catch (e: NumberFormatException) {
-                        println("Введите корректное место (1, 100): ")
-                        place = readln()
-                        continue
                     }
+                    println("Введите корректное место (1, 100): ")
+                    place = readln()
+                    continue
                 }
-                system.sellTicket(sessionDate, placeNum)
+                println(system.sellTicket(sessionDate, placeNum))
+            }
+
+            "2" -> {
+                println("Введите id билета, который хотите вернуть: ")
+                var input : String = readln()
+                while (!isUInt(input)) {
+                    println("Введите корректное id: ")
+                    input = readln()
+                }
+                var ticketId : UInt = input.toUInt()
+                println(system.returnTicket(ticketId))
+            }
+
+            "3" -> {
+                println("Введите id билета посетителя: ")
+                var input : String = readln()
+                while (!isUInt(input)) {
+                    println("Введите корректное id: ")
+                    input = readln()
+                }
+                var ticketId : UInt = input.toUInt()
+                println(system.tagVisitor(ticketId))
+            }
+
+            "4" -> {
+                println("Введите название фильма: ")
+                var movieName : String = readln()
+                println("Введите длительность фильма (в минутах или в формате HH:mm): ")
+                var input : String = readln()
+                while (!isDurationValid(input) && !isUInt(input)) {
+                    println("Введите корректную длительность фильма (в минутах или в формате HH:mm): ")
+                    input = readln()
+                }
+                var duration : UInt = input.toUInt()
+                println(system.addMovie(movieName, duration))
+            }
+
+            else -> {
+                println("Введена неверная команда. Пожалуйста, выберите одну из представленных вариантов. ")
             }
         }
-
-
     }
 }
