55b58f031fdf81e4bd69d9132f7651dc13b87ce9
EvgenBolat <khavronich12@gmail.com>
2023-12-12 15:20:32 +0300

    Changed System & Main

--- src/main/kotlin/System.kt
+++ src/main/kotlin/System.kt
@@ -3,9 +3,14 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import java.io.File
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class System() {
     private var countOfCreatedObjects: Int = 0
+
+    private fun clearSessions(){
+
+    }
     private fun generateId(): UInt{
         return (LocalDateTime.now().year.toString() + LocalDateTime.now().month.toString() +
                 LocalDateTime.now().hour.toString() + LocalDateTime.now().minute.toString() +
@@ -39,7 +44,7 @@ class System() {
     }
 
     fun sellTicket(date: String, place: UInt): String{
-        var sessions = Json.decodeFromString<Array<Session>>(File(sessionsFile).readText(Charsets.UTF_8))
+        val sessions = Json.decodeFromString<Array<Session>>(File(sessionsFile).readText(Charsets.UTF_8))
         for (session in sessions){
             if (session.date == date){
                 if (session.places[place.toInt() - 1] == 0.toByte()){
@@ -58,11 +63,52 @@ class System() {
     }
 
     fun returnTicket(ticketId: UInt) : String {
-        return ("")
+        val tickets = Json.decodeFromString<Array<Ticket>>(File(ticketsFile).readText(Charsets.UTF_8)).toMutableList()
+        for (ticket in tickets){
+            if (ticket.id == ticketId){
+                tickets -= ticket
+                val ticketsArr = tickets.toTypedArray()
+                File(ticketsFile).writeText(Json.encodeToString<Array<Ticket>>(ticketsArr))
+                return "Билет возвращён"
+            }
+        }
+        return ("Билета с данным ID не существует, повторите попытку с корректным билетом")
     }
 
     fun tagVisitor(ticketId: UInt) : String {
-        return ("")
+        val tickets = Json.decodeFromString<Array<Ticket>>(File(ticketsFile).readText(Charsets.UTF_8))
+        for (ticket in tickets){
+            if (ticket.id == ticketId){
+                val sessionID = ticket.sessionId
+                val sessions = Json.decodeFromString<Array<Session>>(File(sessionsFile).readText(Charsets.UTF_8))
+                for (session in sessions){
+                    if (sessionID == session.id){
+                        val movieId = session.movieId
+                        val movies = Json.decodeFromString<Array<Movie>>(File(moviesFile).readText(Charsets.UTF_8))
+                        for (movie in movies){
+                            if (movieId == movie.id){
+                                var dateStart = LocalDateTime.parse(session.date, DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"))
+                                val dateEnd = dateStart.plusSeconds( movie.duration.toLong());
+                                dateStart = dateStart.minusMinutes(15)
+                                val currDate = LocalDateTime.now()
+                                if (currDate < dateStart) {
+                                    return "Отметка происходит за 15 минут до начала фильма. Попробуйте позже"
+                                }
+                                if (currDate >= dateEnd)
+                                    return "Данный сеанс закончился, вы не успели."
+                                }
+                            if(session.places[ticket.place.toInt() - 1] == 2.toByte()){
+                                return "Человек уже отмечен"
+                            }
+                            if(session.places[ticket.place.toInt() - 1] == 1.toByte()){
+                                return "Человек успешно отмечен"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return ("Билета с данным ID не существует, повторите попытку с корректным билетом")
     }
 
     fun showPlaces(date: String) : String {
